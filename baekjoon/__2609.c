6ee8a682baa700aef9c4ba1e42ca471bd578edd0
Jongmin Lee <darkcruel12@naver.com>
2020-03-18 23:30:01 +0900

    03/18

--- baekjoon/__2609.c
+++ baekjoon/__2609.c
@@ -1,46 +1,16 @@
 #include<stdio.h>
-#include<stdbool.h>
 
-bool isprime(int n)
+int gcd(int a,int b)
 {
-    bool result = true;
-    if(n==1)
-    {
-        result = false;
-    }
-    else if(n==2||n==3)
-    {
-        result = true;
-    }
-    else
-    {
-        for(int i=2;i*i<=n;i++)
-        {
-            if(n%i==0)
-            {
-                result = false;
-            }
-        }
-    }
-    return result;
+    if(b==0) return a;
+    else return gcd(b , a%b);
 }
-
 
 int main()
 {
     int N,M;
-    int gcd = 1;
-    int lcm = 1;
     scanf("%d %d",&N,&M);
-    int i=2;
-    int j=2;
-    while(i<N && i<M)
-    {
-        if(N%i==0&&M%i==0)
-        {
-            gcd = i;
-        }
-        i = i+1;
-    }
-    printf("%d\n",i);
+    printf("%d\n",gcd(N,M));
+    printf("%d",N*M/gcd(N,M));
+    return 0;
 }
